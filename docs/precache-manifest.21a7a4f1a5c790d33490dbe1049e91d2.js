b2cf6df53f9461e057a94c5a838f78f18f664eb8
Kayla Jean Crane <kayla.crane@gmail.com>
2020-07-28 08:51:56 +0200

    Add intial file setup and docs for publishing

---
+++ docs/precache-manifest.21a7a4f1a5c790d33490dbe1049e91d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a1cd7dbfa4aff426c90404e78bbe29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88e37c4cd555f44f734b",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc4b9171014db4b7e39a",
+    "url": "./static/js/2.2a1abe38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2a1abe38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e37c4cd555f44f734b",
+    "url": "./static/js/main.7e85dfb1.chunk.js"
+  },
+  {
+    "revision": "58a87d46cc0b25c73487",
+    "url": "./static/js/runtime-main.4fbaedb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
